ca33e014df3e035c05713f979e176df25645b336
SakhnoAndrey <Andrey.Sakhno@gmail.com>
2020-01-29 12:53:47 +0200

    Solved strings module

--- strings.py
+++ strings.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import textwrap
+import string
 
 
 def swap_case():
@@ -64,5 +66,122 @@ def string_validators():
     print("\n".join([str(any(i)) for i in rotated]))
 
 
+def text_alignments():
+    # Replace all ______ with rjust, ljust or center.
+
+    thickness = int(input())  # This must be an odd number
+    c = 'H'
+
+    # Top Cone
+    for i in range(thickness):
+        print((c * i).rjust(thickness - 1) + c + (c * i).ljust(thickness - 1))
+
+    # Top Pillars
+    for i in range(thickness + 1):
+        print((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
+
+    # Middle Belt
+    for i in range((thickness + 1) // 2):
+        print((c * thickness * 5).center(thickness * 6))
+
+        # Bottom Pillars
+    for i in range(thickness + 1):
+        print((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
+
+        # Bottom Cone
+    for i in range(thickness):
+        print(((c * (thickness - i - 1)).rjust(thickness) + c + (c * (thickness - i - 1)).ljust(thickness)).rjust(
+            thickness * 6))
+
+
+def text_wrap():
+    # Input
+    string = input()
+    max_width = int(input())
+    # Function
+    result = textwrap.fill(string, max_width)
+    return result
+
+
+def designer_door_mat():
+    # Input
+    n, m = map(int, input().rstrip().split())
+    pattern = [(".|." * (1 + 2 * i)).center(m, "-") for i in range(n // 2)]
+    print("\n".join(pattern + ["WELCOME".center(m, "-")] + pattern[::-1]))
+
+
+def string_formatting():
+    # Input
+    n = int(input())
+    # Function
+    width = len("{0:b}".format(n))
+    for i in range(1, n + 1):
+        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
+
+
+def alphabet_rangoli():
+    # Input
+    n = int(input())
+    # Function
+    length = n * 4 - 3
+    alphabet = string.ascii_lowercase
+    arr = []
+    for i in range(n):
+        s = "-".join(alphabet[i:n])
+        s = s[:0:-1] + s
+        arr.append(s.center(length, "-"))
+    arr = arr[:0:-1] + arr
+    result = "\n".join(arr)
+    print(result)
+
+
+def capitalize():
+    # Input
+    s = input()
+    # Function
+    s = " ".join([word.capitalize() for word in s.split(" ")])
+    return s
+
+
+def minion_game():
+    # Input
+    s = input()
+    # Function
+    l = len(s)
+    consonants_word = 0
+    vowels_word = 0
+    vowels_set = set("aeiou".upper())
+    for i in range(l):
+        if s[i] in vowels_set:
+            vowels_word += l - i
+
+        else:
+            consonants_word += l - i
+    if vowels_word > consonants_word:
+        print("Kevin {0}".format(vowels_word))
+    elif vowels_word < consonants_word:
+        print("Stuart {0}".format(consonants_word))
+    else:
+        print("Draw")
+
+
+def merge_tools():
+    # Input
+    s = input()
+    k = int(input())
+    # Function
+    n = len(s)
+    arr = []
+    for i in range(0, n, k):
+        substring = s[i:i + k]
+        word = ""
+        for char in substring:
+            if char not in word:
+                word += char
+        arr.append(word)
+    s = "\n".join(arr)
+    print(s)
+
+
 if __name__ == '__main__':
-    string_validators()
+    merge_tools()
