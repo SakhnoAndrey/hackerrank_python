c0c67ad1cfbc7d21078975eca950b1ea0b236fe8
SakhnoAndrey <Andrey.Sakhno@gmail.com>
2020-02-09 12:19:17 +0200

    Built_in

--- built_ins.py
+++ built_ins.py
@@ -6,4 +6,31 @@ def zipped():
     print("\n".join(map(str, average_student)))
 
 
-zipped()
+def func_input():
+    x, k = map(int, input().strip().split())
+    print(eval(input().replace("x", str(x))) == k)
+
+
+def evaluation():
+    eval(input())
+
+
+def athlete_sort():
+    n, m = map(int, input().strip().split())
+    arr = [list(map(int, input().strip().split())) for _ in range(n)]
+    k = int(input().strip())
+    # NEED TO DONE IT
+    [print(" ".join(map(str, row))) for row in sorted(arr, key=lambda r: r[k])]
+
+
+def any_or_all():
+    n, arr = int(input().strip()), list(map(int, input().strip().split()))
+    print(any([str(el) == str(el)[::-1] for el in arr]) if all([el >= 0 for el in arr]) else False)
+
+
+def ginortS():
+    print(*(sorted(input(), key=lambda x: (x.isdigit(), x.isdigit() and int(x) % 2 == 0, x.isupper(), x.islower(), x))),
+          sep='')
+
+
+ginortS()
