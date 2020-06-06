4a30712a454a675589ebc8a26d26536f8eb9c724
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-06-06 18:32:40 +0300

    XML2 - Find the Maximum Depth

--- hr_xml.py
+++ hr_xml.py
@@ -2,6 +2,7 @@ import sys
 import xml.etree.ElementTree as etree
 
 
+# XML 1 - Find the Score
 def get_attr_number():
     # Input
     data = ""
@@ -14,4 +15,29 @@ def get_attr_number():
     return sum(len(item.attrib) for item in node.iter())
 
 
-print(get_attr_number())
+# XML2 - Find the Maximum Depth
+maxdepth = 0
+
+
+def input_xml():
+    # Input
+    global maxdepth
+    n = int(input())
+    data = ""
+    for _ in range(n):
+        data += input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(data))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
+
+
+def depth(elem, level):
+    global maxdepth
+    level += 1
+    if maxdepth < level:
+        maxdepth = level
+    for item in elem:
+        depth(item, level)
+
+
+input_xml()
