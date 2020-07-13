67f7a5a55f3ebd148e54ad0727be1d505e55feba
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-07-13 10:19:43 +0300

    Transpose and flatten numpy

--- numeric_python.py
+++ numeric_python.py
@@ -20,4 +20,18 @@ def shape_reshape():
     print(numpy.reshape(np_arr, (3, 3)))
 
 
-shape_reshape()
+# Transpose and Flatten
+def transpose_flatten():
+    # Input
+    n, m = map(int, input().rstrip().split())
+    arr = []
+    for i in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    # Function
+    np_arr = numpy.array(arr)
+    print(np_arr.transpose())
+    print(np_arr.flatten())
+
+
+transpose_flatten()
