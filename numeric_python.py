8aa4741987641c07aac4324a2f6ef1ef6622dd99
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-07-13 08:08:02 +0300

    Shape and reshape numpy objects

--- numeric_python.py
+++ numeric_python.py
@@ -1,6 +1,7 @@
 import numpy
 
 
+# Arrays
 def arrays():
     # Input
     arr = input().rstrip().split(" ")
@@ -9,4 +10,14 @@ def arrays():
     return numpy.array(arr[::-1], float)
 
 
-print(arrays())
+# Shape and Reshape
+def shape_reshape():
+    # Input
+    arr = list(map(int, input().rstrip().split()))
+
+    # Function
+    np_arr = numpy.array(arr)
+    print(numpy.reshape(np_arr, (3, 3)))
+
+
+shape_reshape()
