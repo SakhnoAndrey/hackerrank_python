a81a65b5ef5e2586557d8254b07e051afece53bd
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-09-04 13:42:23 +0300

    Default arguments

--- debugging.py
+++ debugging.py
@@ -1,3 +1,4 @@
+# Words score
 def is_vowel(letter):
     return letter in ["a", "e", "i", "o", "u", "y"]
 
@@ -16,6 +17,43 @@ def score_words(words):
     return score
 
 
-n = int(input())
-arr_words = input().split()
-print(score_words(arr_words))
+# n = int(input())
+# arr_words = input().split()
+# print(score_words(arr_words))
+
+
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+def print_from_stream(n, stream=None):
+    if stream is None:
+        stream = EvenStream()
+    for _ in range(n):
+        print(stream.get_next())
+
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
