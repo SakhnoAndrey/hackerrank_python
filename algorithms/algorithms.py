1e3334fd16fd6507df60095c27be009f80c4ec3b
Andrey Sakhno <andrey.sakhno@gmail.com>
2025-07-04 12:22:41 +0300

    Gemstones

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,4 +1,5 @@
 import math
+from copy import copy
 from email.charset import add_charset
 from itertools import combinations
 
@@ -236,5 +237,15 @@ def countingSort2(arr):
     return result
 
 
+def gemstones(arr: list):
+    # Write your code here
+    d = {k: 0 for k in range(1, 27)}
+    data = [copy(d) for _ in range(len(arr))]
+    for i, item in enumerate(arr):
+        for ch in set(item):
+            data[i][ord(ch) - 96] = 1
+    return sum([all(elem) for elem in zip(*[list(item.values()) for item in data])])
+
+
 if __name__ == '__main__':
-    print(countingSort2(str_to_list_of_int("63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33")))
+    result = gemstones(['abcdde', 'baccd', 'eeabg'])
