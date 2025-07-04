a71b3eb8d13b92bab210b0695c88620e54c939a3
Andrey Sakhno <andrey.sakhno@gmail.com>
2025-07-04 17:13:13 +0300

    Beautiful Binary String

--- helpers.py
+++ helpers.py
@@ -1,2 +1,11 @@
+from typing import List, Any
+
+
 def str_to_list_of_int(s: str) -> list:
     return list(map(int, s.strip().split(' ')))
+
+def two_dimensional_array_int_str(s) -> list[list[int | Any]]:
+    return [
+        [int(v) if i == 0 else v for i, v in enumerate(item.split(' '))]
+        for item in s.strip("\n").strip().split('\n')
+    ]
