f0ee82a91e98058d3e8be89e0b43808483e116b8
Andrey Sakhno <andrey.sakhno@gmail.com>
2025-07-07 18:01:54 +0300

    Determining DNA Health v4

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -447,8 +447,64 @@ def determiningDNAhealthV2(genes: list = None, health: list = None, s: int = 0, 
     print(f"{min_score} {max_score}")
 
 
+from math import inf
+from bisect import bisect_left as bLeft, bisect_right as bRight
+from collections import defaultdict
+
+
+
+def getHealth(subs, gMap, seq, first, last, largest):
+    h, ls = 0, len(seq)
+
+    for f in range(ls):
+        for j in range(1, largest+1):
+            if f+j > ls:
+                break
+            sub = seq[f:f+j]
+            if sub not in subs:
+                break
+            if sub not in gMap:
+                continue
+            ids, hs = gMap[sub]
+            h += hs[bRight(ids, last)]-hs[bLeft(ids, first)]
+    return h
+
+
+def determiningDNAhealthV4(genes: list = None, health: list = None, s: int = 0, first: int = 0, last: int = 0, d: str = None):
+    strands = []
+    if d is not None:
+        strands = [[first, last, d]]
+
+    if any(x is None for x in [genes, health]):
+        arr = dna_input()
+        genes = arr[0]
+        health = arr[1]
+        strands = arr[2:]
+
+
+    gMap = defaultdict(lambda: [[], [0]])
+    subs = set()
+    for i, gene in enumerate(genes):
+        gMap[gene][0].append(i)
+        for j in range(1, min(len(gene), 500)+1):
+            subs.add(gene[:j])
+
+    for v in gMap.values():
+        for i, ix in enumerate(v[0]):
+            v[1].append(v[1][i]+health[ix])
+
+    largest = max(list(map(len, genes)))
+    hMin, hMax = inf, 0
+
+    for first, last, d in strands:
+        h = getHealth(subs, gMap, d, first, last, largest)
+        hMin, hMax = min(hMin, h), max(hMax, h)
+
+    print(f"{hMin} {hMax}")
+
+
 
 
 if __name__ == '__main__':
-    result = determiningDNAhealthV2()
+    result = determiningDNAhealthV4()
     print(result)
